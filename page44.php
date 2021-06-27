350db1f9cefcc1ded3e32dc90e88b681b119f999
shaheenalamjony <shaheenalamjony9@gmail.com>
2021-06-28 03:18:04 +0600

    mid project upload

---
+++ page44.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+
+<?php 
+include 'h.php';
+echo "<br>";
+include 'banner.html';echo "<br>";
+include 'speech.html';echo "<br>";
+include 'video.html';echo "<br>";echo "<br>";
+include 'footer.php';
+?>    
+</body>
+</html>
